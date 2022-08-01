66a47e971fbb86814980ccaef7ece02a471aa1dd
João Cardoso <joaolucas.cardoso02@gmail.com>
2022-07-31 21:33:29 -0300

    feat: add simple controller to send email

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using MailKit.Security;
+
+namespace SimpleEmailApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult SendEmail(string body) {
+        var email = new MimeMessage();
+        email.From.Add(MailboxAddress.Parse("chester.hoppe36@ethereal.email"));
+        email.To.Add(MailboxAddress.Parse("chester.hoppe36@ethereal.email"));
+        email.Subject = "Test Email Subject";
+        email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+        using var smtp = new SmtpClient();
+        smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+        smtp.Authenticate("chester.hoppe36@ethereal.email", "hJNF9qemDBm8xqGqPp");
+
+        smtp.Send(email);
+        smtp.Disconnect(true);
+
+        return Ok();
+    }
+}
+
